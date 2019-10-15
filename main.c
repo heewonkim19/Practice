d668d2db8acc197bbd3ce6e1a85980faf4173f85
heewonkim19 <55428337+heewonkim19@users.noreply.github.com>
2019-10-15 21:09:23 +0900

    addition program

--- main.c
+++ main.c
@@ -2,43 +2,18 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-/* The number of Days of Month*/ 
-int main(int argc, char *argv[]) {
-	
-	int month, days;
-	printf("input a month");
-	scanf("%d", &month);
 
-	switch (month)
+int main(int argc, char *argv[]) {
+	int n;
+	int i;
+	int sum=0;
+	printf ("input a number:");
+	scanf("%d",&n);
+	for (i=1; i<=n; i++)
 	{
-		case 1:
-			printf("Day is 31.");
-		break;
-		case 3:
-			printf("Day is 31.");
-		break;
-		case 5:
-			printf("Day is 31.");
-		break;
-		case 7:
-			printf("Day is 31.");
-		break;
-		case 8:
-			printf("Day is 31.");
-		break;
-		case 10:
-			printf("Day is 31.");
-		break;
-		case 12:	
-			printf("Day is 31.");
-		break;
-		case 2:
-			printf("Day is 28.");
-		break;
-		default:
-			printf("Day is 30.");
-				
+		sum+=i;
 	}
+	printf("result is %d",sum);
 	return 0;
 }
 
