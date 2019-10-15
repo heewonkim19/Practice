16cda0fe78ffe6fd72e0bc05191a43a1836f5eac
heewonkim19 <55428337+heewonkim19@users.noreply.github.com>
2019-10-15 20:58:45 +0900

    The number of Days of Months

--- main.c
+++ main.c
@@ -2,24 +2,43 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-/* Calculation factorial */
-
-int factorial (int n)	
-{	int i;
-	int result=1;
-	for (i=1; i<=n; i++)
-		result*=i;
-	return result;
-}
-
+/* The number of Days of Month*/ 
 int main(int argc, char *argv[]) {
 	
-	int n;
-	printf ("input 'n'");
-	scanf ("%d",&n);
-	
-	printf ("Result is %d", factorial (n));
-	
+	int month, days;
+	printf("input a month");
+	scanf("%d", &month);
+
+	switch (month)
+	{
+		case 1:
+			printf("Day is 31.");
+		break;
+		case 3:
+			printf("Day is 31.");
+		break;
+		case 5:
+			printf("Day is 31.");
+		break;
+		case 7:
+			printf("Day is 31.");
+		break;
+		case 8:
+			printf("Day is 31.");
+		break;
+		case 10:
+			printf("Day is 31.");
+		break;
+		case 12:	
+			printf("Day is 31.");
+		break;
+		case 2:
+			printf("Day is 28.");
+		break;
+		default:
+			printf("Day is 30.");
+				
+	}
 	return 0;
 }
 
