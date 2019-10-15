76e60adfa07f5f54ac266de612b6451040edc835
heewonkim19 <55428337+heewonkim19@users.noreply.github.com>
2019-10-15 20:37:28 +0900

    calculation factorial

--- main.c
+++ main.c
@@ -2,15 +2,23 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-/* ASCII_CODE */
+/* Calculation factorial */
+
+int factorial (int n)	
+{	int i;
+	int result=1;
+	for (i=1; i<=n; i++)
+		result*=i;
+	return result;
+}
+
 int main(int argc, char *argv[]) {
 	
-	int ASCII_CODE;
+	int n;
+	printf ("input 'n'");
+	scanf ("%d",&n);
 	
-	printf("input a ASCII CODE:");
-	scanf("%d", &ASCII_CODE);
-	printf("letter: \'%c\'", ASCII_CODE);
-
+	printf ("Result is %d", factorial (n));
 	
 	return 0;
 }
