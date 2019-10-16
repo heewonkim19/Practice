6a5fc0078bfc3f1248d1d6f3867c579d08530285
heewonkim19 <55428337+heewonkim19@users.noreply.github.com>
2019-10-17 08:40:29 +0900

    calculation program

--- main.c
+++ main.c
@@ -2,18 +2,39 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	int n;
-	int i;
-	int sum=0;
-	printf ("input a number:");
-	scanf("%d",&n);
-	for (i=1; i<=n; i++)
+/* calculation program */
+int main(int argc, char *argv[]) 
+{
+	int x,y,result;
+	char op;
+	
+	printf("input equation:\n");
+	scanf("%d %c %d", &x, &op, &y);
+	
+	switch (op)
 	{
-		sum+=i;
-	}
-	printf("result is %d",sum);
+		case '+':
+			result = x + y;
+			break;
+		case '-':
+			result = x - y;
+			break;
+		case '*':
+			result = x * y;
+  			break;		
+		case '/':
+			result = x / y;
+			break;
+		case '%':
+			result = x % y;
+			break;
+		default:
+			printf("Unsupported operator.");
+			break;
+	}	
+	printf("%d %c %d = %d\n", x, op, y, result);
 	return 0;
+				
+		  
 }
 
