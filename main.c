45d79ab997436347f3116dabc5ff1208d66e14ce
heewonkim19 <55428337+heewonkim19@users.noreply.github.com>
2019-10-19 18:37:48 +0900

    Rock_paper_scissors

--- main.c
+++ main.c
@@ -2,48 +2,39 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-/* Change Calculation Program */
+
+/* Rock_Paper_scissors */
 int main(int argc, char *argv[]) 
 {
-	int price;
 	int user;
-	int change;
-	int c10000, c5000, c1000, c500, c100, c50, c10;
-	printf("input a price:");
-	scanf("%d", &price);
-	printf("input a money:");
-	scanf("%d", &user);	  
-	change = user - price;
+	int computer;
 	
-	c10000 = change / 10000;
-	change %= 10000;
+	computer = (rand()%3+1);
+	printf("choice a number(1:scissors, 2:rock, 3:paper): ");
+	scanf("%d", &user);
 	
-	c5000 = change / 5000;
-	change %= 5000;
+	if(user+computer!= 4)
+	{
+		if (user>computer)
+			printf("winner is user.\n");
+		else if (user<computer)
+			printf("winner is computer.\n");
+		else
+			printf("user = computer.\n");
+	}
+	else if (user+computer == 4)
+	{
+		if (user<computer)
+			printf("winner is user.\n");
+		else if (user>computer)
+			printf("winner is computer.\n");
+		else
+			printf("user = computer.\n");
+	}	 
 	
-	c1000 = change / 1000;
-	change %= 1000;
+	printf("user= %d | computer= %d", user, computer);
 	
-	c500 = change / 500;
-	change %= 500;
-	
-	c100 = change / 100;
-	change %= 100;
-	
-	c50 = change / 50;
-	change %= 50;
-	
-	c10 = change / 10;
-	change %= 10;
-	
-	printf("10000 : %d\n", c10000);
-	printf("5000 : %d\n", c5000);
-	printf("1000 : %d\n", c1000);
-	printf("500 : %d\n", c500);
-	printf("100 : %d\n", c100);
-	printf("50 : %d\n", c50);
-	printf("10 : %d\n", c10);
-	
-	return 0;
-}
 
+	return 0;
+
+}
